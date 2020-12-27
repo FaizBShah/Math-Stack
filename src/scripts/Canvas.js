b78e130b021c1554c9ce18ad2d86553eb619c0cd
AmitManna99 <manna.amit99@gmail.com>
2020-12-27 18:14:56 +0530

    low-fdility matrix print implemented

--- src/scripts/Canvas.js
+++ src/scripts/Canvas.js
@@ -4,15 +4,25 @@ class CustomCanvas {
 
   // eslint-disable-next-line no-useless-constructor
   constructor() {
-    
+
   }
 
   showMatrix(matrix, rows, cols) {
+    var table = `<table>`
+    for (var i = 0; i < rows; i++) {
+      table += `<tr>`;
+      for (var j = 0; j < cols; j++) {
+        table += `<td>${matrix[i][j]}</td>`
+      }
+      table += `</tr>`;
+    }
+    table += `</table>`;
 
+    document.body.innerHTML = document.body.innerHTML + table;
   }
 
   showVariable(x) {
-
+    document.body.innerHTML = document.body.innerHTML + x;
   }
 }
 
