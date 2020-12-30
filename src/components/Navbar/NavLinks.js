6248853a3c4ab0631682b8f42dbc96af166dc8a1
Abhinav Kumar <work.abhinavkr@gmail.com>
2020-12-30 16:19:11 +0530

    input boxes

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -8,8 +8,8 @@ const NavLinks = (props) => {
     <div>
       <a href="#" data-target="slide-out" className="sidenav-trigger right" id="nav-menu"><i className="material-icons grey-text text-lighten-3">menu</i></a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#" className="nav-link grey-text text-lighten-3"><b>user : </b> {props.auth.email}</a></li>
-        <li><a href="#" className="nav-link grey-text text-lighten-3" onClick={props.signOut}><b>Log Out</b></a></li>
+        <li><a href="#" className="nav-link"><i className="material-icons">account_circle</i> {props.auth.email}</a></li>
+        <li><a href="#" className="nav-link" onClick={props.signOut}>LOG OUT <i className="material-icons">exit_to_app</i></a></li>
       </ul>
     </div>
   )
